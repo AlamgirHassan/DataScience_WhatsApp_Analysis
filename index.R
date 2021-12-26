dbba3cb26c9bd3269461f5ab3c64cb5776205874
AlamgirHassan <89346524+AlamgirHassan@users.noreply.github.com>
2021-12-27 00:21:13 +0500

    Add files via upload

---
+++ index.R
@@ -0,0 +1,54 @@
+library(stringr)
+library(ggplot2)
+library("rwhatsapp")
+library("dplyr")
+library("lubridate")
+library("tidyr")
+library("tidytext")
+library("stopwords")
+chat_data <- rwa_read("C:\\Users\\HP\\Desktop\\practice\\whatsapp_chat.txt") %>% 
+  filter(!is.na(author)) 
+
+chat_data %>%
+  
+  mutate(day = date(time)) %>%
+  count(day) %>%
+  ggplot(aes(x = day, y = n)) +
+  geom_bar(stat = "identity") +
+  ylab("") + xlab("") +
+  ggtitle("Number of messages with respect to days")
+
+
+
+chat_data %>%
+  unnest(emoji) %>%
+  count(author, emoji, sort = TRUE) %>%
+  group_by(author) %>%
+  top_n(n = 6, n) %>%
+  ggplot(aes(x = reorder(emoji, n), y = n, fill = author)) +
+  geom_col(show.legend = FALSE) +
+  ylab("") +
+  xlab("") +
+  coord_flip() +
+  facet_wrap(~author, ncol = 2, scales = "free_y")  +
+  ggtitle("Mostly used emojies in Chat")
+
+
+
+chat_data %>%
+  unnest_tokens(input = text,
+                output = word) %>%
+  count(author, word, sort = TRUE) %>%
+  group_by(author) %>%
+  top_n(n = 4, n) %>%
+  ggplot(aes(x = reorder_within(word, n, author), y = n, fill = author)) +
+  geom_col(show.legend = FALSE) +
+  ylab("") +
+  xlab("") +
+  coord_flip() +
+  facet_wrap(~author, ncol = 2, scales = "free_y") +
+  scale_x_reordered() +
+  ggtitle("Mostly used words in Chat")
+
+
+
